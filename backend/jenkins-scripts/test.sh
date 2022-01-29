1becddc8a471628176507e34dff00d2bb2b50baf
srinu7150 <codesrinivas7150@gmail.com>
2022-01-29 12:05:13 +0530

    corrected test.sh

--- backend/jenkins-scripts/test.sh
+++ backend/jenkins-scripts/test.sh
@@ -5,8 +5,9 @@ echo "***testing backend***"
 echo "*********************"
 
 docker run \
---env DB_USER = "$DB_CREDENTIALS_USR" \
---env DB_PASSWORD = "$DB_CREDENTIALS_PSW" \
---env DB_NAME = "$DB_NAME" \
---entrypoint npm test \
-backend:$BUILD_TAG
+--rm \
+--env DB_USER=$DB_CREDENTIALS_USR \
+--env DB_PASSWORD=$DB_CREDENTIALS_PSW \
+--env DB_NAME=$DB_NAME \
+backend:$BUILD_TAG \
+npm test
