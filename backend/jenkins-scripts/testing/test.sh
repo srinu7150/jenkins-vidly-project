25c141b0b655ce2a79211ec9cbb3bc3ab2cdd322
srinu7150 <codesrinivas7150@gmail.com>
2022-01-29 16:01:38 +0530

    moved wait-for and entrypoint to backend

--- backend/jenkins-scripts/testing/test.sh
+++ backend/jenkins-scripts/testing/test.sh
@@ -11,7 +11,6 @@ docker run \
 --env DB_USER=$DB_CREDENTIALS_USR \
 --env DB_PASSWORD=$DB_CREDENTIALS_PSW \
 --env DB_NAME=$DB_NAME \
--v /backend/node_modules \
 backend:$BUILD_NUMBER \
 ./docker-entrypoint.sh
 
