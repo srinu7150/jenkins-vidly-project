2acecf520248439fd25d12419cc484a55a47c39d
srinu7150 <codesrinivas7150@gmail.com>
2022-01-31 19:33:46 +0530

    added push option to dockerhub

--- backend/jenkins-scripts/deploying/deploy.sh
+++ backend/jenkins-scripts/deploying/deploy.sh
@@ -10,3 +10,5 @@ echo "loging into dockerhub"
 docker tag backend:$BUILD_NUMBER srinu7150/jenkins-vidly-backend:$BUILD_NUMBER
 
 echo $DOCKER_CREDENTIALS_PSW | docker login -u $DOCKER_CREDENTIALS_USR --password-stdin
+
+docker push srinu7150/jenkins-vidly-backend:$BUILD_NUMBER
