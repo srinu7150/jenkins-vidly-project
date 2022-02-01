d5e9b385eb35eaf6b814bf2b57a41537e8a786f2
srinu7150 <codesrinivas7150@gmail.com>
2022-02-01 15:39:04 +0530

    nginx config working with templating

--- frontend/nginx_config/templating_script.sh
+++ frontend/nginx_config/templating_script.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-envsubst < ./web.conf > /etc/nginx/conf.d/default.conf
+envsubst < /nginx_config/location.conf > /etc/nginx/conf.d/default.conf
+cat /nginx_config/web.conf >> /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
+
+
+# envsubst < /nginx_config/web.conf > /etc/nginx/conf.d/default.conf
+# nginx -g 'daemon off;'
